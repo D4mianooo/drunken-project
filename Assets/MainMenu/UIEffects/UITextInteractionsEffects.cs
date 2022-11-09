aa60f8162a63dcf17fc3feb1c3c6781ebf6f919b
XenkQ <blaszynski.konrad@gmail.com>
2022-11-09 21:05:52 +0100

    MainMenu update
    Stylizing main menu part 1

---
+++ Assets/MainMenu/UIEffects/UITextInteractionsEffects.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+using TMPro;
+
+public class UITextInteractionsEffects : MonoBehaviour
+{
+    [SerializeField] [ColorUsage(true)] private Color32 defaultColor;
+    [SerializeField] [ColorUsage(true)] private Color32 interactionColor;
+
+    [SerializeField] private float defaultTextSize;
+    [SerializeField] private float textSizeAfterInteraction;
+
+    [SerializeField] private TextMeshProUGUI[] allTextToRestart;
+
+    public void OnTextEnterEffect(TextMeshProUGUI buttonText)
+    {
+        ChangeFontColor(buttonText, interactionColor);
+        ChangeFontSize(buttonText, textSizeAfterInteraction);
+    }
+
+    public void OnTextExitEffect(TextMeshProUGUI buttonText)
+    {
+        ChangeFontColor(buttonText, defaultColor);
+        ChangeFontSize(buttonText, defaultTextSize);
+    }
+
+    public void ChangeFontSize(TextMeshProUGUI text, float newSize)
+    {
+        text.fontSize = newSize;
+    }
+
+    public void ChangeFontColor(TextMeshProUGUI text, Color32 color)
+    {
+        text.faceColor = color;
+    }
+
+    public void RestoreAllTextDefaultStyle()
+    {
+        foreach (TextMeshProUGUI text in allTextToRestart)
+        {
+            ChangeFontColor(text, defaultColor);
+            ChangeFontSize(text, defaultTextSize);
+        }
+    }
+}
